f1ad94a9196d023081207fbb1cc3a72dc55d8484
agomez99 <austine.gomez@yahoo.com>
2020-05-22 23:20:16 -0500

    handle alert

--- budget_tracker/src/App.js
+++ budget_tracker/src/App.js
@@ -7,7 +7,6 @@ import uuid from 'uuid/v4'
 
 
 const initialExpenses = [
-
   {id: uuid(),charge: "rent", amount: 1600},
   {id: uuid(),charge: "car payment", amount: 400},
   {id: uuid(),charge: "credit card bill", amount: 1200}
@@ -16,22 +15,72 @@ const initialExpenses = [
 
 
 function App() {
+//state values
+//all expenses, add expenses
 const [expenses, setExpenses] = useState(initialExpenses);
+//single expense
+const [charge, setCharge] = useState("");
+//single amount
+const [amount, setAmount] = useState("");
+//alert
+const [alert,setAlert] = useState({show:false});
+//functionality
+//handle charge
+const handleCharge = e =>{
+  setCharge(e.target.value)
+};
+//handle amount
+const handleAmount = e =>{
+  setAmount(e.target.value)
+};
+
+//handle alert
+const handleAlert =({type, text})=>{
+  setAlert({show:true, type, text});
+  setTimeout(() => {
+    setAlert({show:false})
+  }, 3000)
+}
+
+//hanlde submit
+const handleSubmit = e =>{
+e.preventDefault();
+if(charge !== "" && amount > 0){
+const singleExpense = {id: uuid(), charge, amount};
+setExpenses([...expenses,singleExpense]);
+handleAlert({type:'success', text:'item added' })
+setCharge("");
+setAmount("");
+}else{
+  //handle alert called
+  handleAlert({type: 'danger', text:`charge can't be empty value, value has tyo be > 0 `})
+
+}
+};
+
 
   return ( 
   <>
-  <Alert />
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
   <h1>budget calclulator</h1>
   <main className= "App">
-  <ExpenseForm />
-  <ExpenseList expenses={expenses}/>
+  <ExpenseForm  
+  handleSubmit={handleSubmit}
+  charge={charge} 
+  amount={amount}
+  handleAmount={handleAmount} 
+  handleCharge={handleCharge}
+  />
+  <ExpenseList 
+  expenses={expenses}
+  />
   </main>
 <h1>
   total spending: {" "}
   <span className= "total">
     $
    {expenses.reduce((acc, curr) => {
-    return (acc += curr.amount);
+    return (acc += parseInt(curr.amount));
       },0)}
   </span>
   </h1>
--- budget_tracker/src/components/ExpenseList.js
+++ budget_tracker/src/components/ExpenseList.js
@@ -1,21 +1,29 @@
 import React from "react";
-import Item from "./ExpenseItem";
 import {MdDelete} from 'react-icons/md'
+import ExpenseItem from "./ExpenseItem";
 
 const ExpenseList = ({ expenses }) => {
   return (
     <>
       <ul className="list">
         {expenses.map((expense) => {
-          return <Item key={expense.id} expense={expense} />;
+          return (
+            <ExpenseItem
+              key={expense.id}
+              expense={expense}
+              //handleDelete={handleDelete}
+              //handleEdit={handleEdit}
+            />
+          );
         })}
       </ul>
-      {expenses.length > 0 && <button className = "btn"> 
+      {expenses.length > 0 && (
+      <button className = "btn"> 
       Clear Expenses
       <MdDelete className="btn-icon"/>
-      </button>}
+      </button>
       )}
-      </>
+    </>
   );
 };
 
