21f19ceabb73beab078eb4d8d72175ed8c45eb79
agomez99 <austine.gomez@yahoo.com>
2020-05-23 01:06:07 -0500

    Updates

---
+++ precache-manifest.d9b12d4f91f86e99c583502fe3f16991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963781914eb1bc6676728042273e3a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a5e0a6b92ed3ef9e863",
+    "url": "/static/css/main.acf4bcdb.chunk.css"
+  },
+  {
+    "revision": "a75e3fee0724f7c4652d",
+    "url": "/static/js/2.529f20eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.529f20eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5e0a6b92ed3ef9e863",
+    "url": "/static/js/main.46638723.chunk.js"
+  },
+  {
+    "revision": "17489dcb4101a6227aef",
+    "url": "/static/js/runtime-main.2b3d49f5.js"
+  }
+]);
